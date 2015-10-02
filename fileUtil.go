a180fc33e612df7930c4787ccb94182a607245b8
alesr <alessandro.resta@gmail.com>
2015-10-02 15:47:18 +0100

    new function FindHomeDir

--- fileUtil.go
+++ fileUtil.go
@@ -3,6 +3,7 @@ package fileUtil
 import (
 	"io/ioutil"
 	"log"
+	"os/user"
 )
 
 // ReadFile - A simple file reader.
@@ -12,4 +13,13 @@ func ReadFile(file string) string {
 		log.Fatal("Error on reading file.", err)
 	}
 	return string(data[:len(data)])
+}
+
+// FindUserHomeDir outputs the filepath for home directory.
+func FindUserHomeDir() string {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal("Error on searching for home directory.", err)
+	}
+	return usr.HomeDir
 }
